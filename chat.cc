9a42437e6d066994ea5dcc0e398e536a8246eaa3
Jeremy <jeremylessermore@gmail.com>
2019-10-12 16:21:01 -0600

    Added error checking for IP and port

--- chat.cc
+++ chat.cc
@@ -2,6 +2,8 @@
 #include <unistd.h>  
 #include <stdlib.h>
 #include <ctype.h>
+#include <string>
+#include <algorithm>
 
 
 using namespace std;
@@ -14,11 +16,62 @@ void server(){
 void help(){
     cout << "Welcome to the help menu!" << endl;
 }
+bool checkIfInt(string port){
+    for (size_t  i = 0; i < port.length(); i++){
+        if (isdigit(port[i]) == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+bool checkForPeriods(string IP){
+    int counter = 0;
+    for (size_t  i = 0; i < IP.length(); i++){
+        if (IP[i] == '.'){
+            counter++;
+        }
+    }
+    if (counter == 0){
+        return false;
+    }
+    return true;
+}
+
+bool CheckValidIPNumber(string IP){
+    IP.erase(std::remove(IP.begin(), IP.end(), '.'), IP.end());
+    if (checkIfInt(IP) == false){
+        return false;
+    }
+    return true;
+}
+
+bool checkIP(string IP){
+    if (checkForPeriods(IP) == false){
+        return false;
+    }
+    if (CheckValidIPNumber(IP) == false){
+        return false;
+    }
+    return true;
+}
 
 void client(string port, string IP){
-    cout << "Port : " << port << endl;
-    cout << "IP : " << IP << endl;
+    if (checkIP(IP) == false && checkIfInt(port)==false){
+        cout << "Both IP Address and Port are not valid. Please try again!" << endl;
+        exit (1);
+    }
+    else if (checkIP(IP) == false){
+        cout << "Not a valid IP Address.Please try again!" << endl;
+        exit (1);
+    }
+    else if (checkIfInt(port) == false){
+        cout << "Port must be a number. Please try again!" << endl;
+        exit (1);
+    }
+    
 }
+
 
 
 
